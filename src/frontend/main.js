50257259eee723db175cc133337beb7af6425432
Bhavya Venkat <bhavya.v04@gmail.com>
2025-12-01 18:20:55 -0500

    testing changes

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,6 +1,21 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app.js";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Account from "./pages/account";
+import Wardrobe from "./pages/wardrobe";
+import Home from "./pages/home"; // your landing page
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/wardrobe" element={<Wardrobe />} />
+    </Routes>
+  </BrowserRouter>
+);
